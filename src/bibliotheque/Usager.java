e254ccf0839e4556b2e491ae622ab7bca92d1905
haouach <haouach.azl@gmail.com>
2018-03-14 08:29:27 +0100

    Usager + Usagers

---
+++ src/bibliotheque/Usager.java
@@ -0,0 +1,76 @@
+package bibliotheque;
+
+public class Usager {
+	private String idUsager;
+	private String datenaiss;
+	private String nom;
+	private String prenom;
+	private String address;
+
+	public Usager(String idUsager, String datenaiss, String nom, String prenom, String address) {
+		super();
+		this.idUsager = idUsager;
+		this.datenaiss = datenaiss;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.address = address;
+	}
+
+	public String getIdUsager() {
+		return idUsager;
+	}
+
+	public void setIdUsager(String idUsager) {
+		this.idUsager = idUsager;
+	}
+
+	public String getDatenaiss() {
+		return datenaiss;
+	}
+
+	public void setDatenaiss(String datenaiss) {
+		this.datenaiss = datenaiss;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public boolean equals(Usager o) {
+		if (!(o instanceof Usager)) {
+			return false;
+		}
+		Usager user = (Usager) o;
+		return getIdUsager().equals(user.idUsager);
+
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idUsager == null) ? 0 : idUsager.hashCode());
+		return result;
+	}
+
+}
